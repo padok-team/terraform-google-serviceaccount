bf057a45bdb42bb757ef21d00c8be6036ac94375
spoukke <sacha.bernheim@hey.com>
2022-03-16 20:12:33 +0100

    feat: change key et custom_roles type and improve description

--- outputs.tf
+++ outputs.tf
@@ -1,20 +1,20 @@
 output "project_iam_custom_roles" {
-  description = "Custom roles for the project"
-  value = toset([
+  description = "A list of the created roles."
+  value = [
     for r in google_project_iam_custom_role.this : r.name
-  ])
+  ]
 }
 
-output "service_account_keys" {
-  description = "Service account keys"
-  value = toset([
-    for k in google_service_account_key.this : k.private_key
-  ])
+output "keys" {
+  description = "A map of each of the service accounts' keys."
+  value = {
+    for k, v in google_service_account_key.this : k => google_service_account_key.this[k].private_key
+  }
   sensitive = true
 }
 
-output "service_account_emails" {
-  description = "Service account emails"
+output "emails" {
+  description = "A map of each service accounts' emails."
   value = {
     for k, v in var.service_accounts : k => google_service_account.this[k].email
   }
